34c73c6c2fee537789cf257086fba165d3e97cf6
szter <sthrpx.e@gmail.com>
2017-12-17 22:17:50 -0300

    add day 2

---
+++ 02-clock/resources/js/app.js
@@ -0,0 +1,44 @@
+// Prettier: ctrl+shift+p > Format Document OR Format Selection
+window.requestAnimFrame = (function(){
+    return  window.requestAnimationFrame       ||
+            window.webkitRequestAnimationFrame ||
+            window.mozRequestAnimationFrame    ||
+            function( callback ){
+                window.setTimeout(callback, 1000 / 60);
+            };
+  })();
+
+(function setClock() {
+    // get the elements
+    const secondHand = document.querySelector('.sec-hand');
+    const minutesHand = document.querySelector('.min-hand');
+    const hourHand = document.querySelector('.hour-hand');
+    const allHands = document.querySelectorAll('.hand');
+
+    // get Date
+    const now = new Date();
+    const seconds = now.getSeconds();
+    const mins = now.getMinutes();
+    const hours = now.getHours();
+
+    // convert to degrees
+    const secondsDegrees = ((seconds / 60) * 360) + 90;
+    const minsDegrees = ((mins / 60) * 360) + 90;
+    const hoursDegrees = ((hours / 12) * 360) + 90;
+    
+    // rotate hands
+    secondHand.style.transform = `rotate(${secondsDegrees}deg)`;
+    minutesHand.style.transform = `rotate(${minsDegrees}deg)`;
+    hourHand.style.transform = `rotate(${hoursDegrees}deg)`;
+    
+    // remove transition when hands reach 90 deg and re-apply it afterwards
+    if(secondsDegrees === 90) {
+        allHands.forEach(hand => hand.style.transition = 'none')
+    } else {
+        allHands.forEach(hand => hand.style.transition = '')
+    }
+
+    setInterval(setClock, 1000);
+})();
+
+
