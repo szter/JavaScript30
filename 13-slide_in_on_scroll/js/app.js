871b9703bce0688dce7e0eb77e3ae07827bfab4f
szter <sthrpx.e@gmail.com>
2018-01-27 13:57:26 -0300

    add app.js

--- 13-slide_in_on_scroll/js/app.js
+++ 13-slide_in_on_scroll/js/app.js
@@ -13,3 +13,31 @@ function debounce(func, wait = 20, immediate = true) {
         if (callNow) func.apply(context, args);
     };
 }
+
+const sliderImages = document.querySelectorAll('.slide-in');
+
+function checkSlide(e) {
+    // console.log(e);
+    // console.log(window.scrollY);
+
+    sliderImages.forEach(sliderImg => {
+        // halfway through the image
+        const slideInAt = (window.scrollY + window.innerHeight) + sliderImg.height / 2;
+        // console.log(slideInAt);
+        
+        // botton of the image
+        const imgBotton = sliderImg.offsetTop + sliderImg.height;
+        console.log({imgBotton});
+        
+        const isHalfShown = slideInAt > sliderImg.offsetTop;
+        console.log({isHalfShown});
+        
+        const isNotScrolledPast = window.scrollY < imgBotton;
+        console.log([isNotScrolledPast]);
+        
+        if (isHalfShown && isNotScrolledPast) sliderImg.classList.add('active');
+        else sliderImg.classList.remove('active');
+    });
+}
+
+window.addEventListener('scroll', debounce(checkSlide));
